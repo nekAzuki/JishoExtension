aac3aea51020e37141dc13ad5e62de2b877b764c
nekAzuki <lanhaw@uw.edu>
2017-08-03 00:11:57 -0700

    simple ver done

--- extension/script.js
+++ extension/script.js
@@ -3,13 +3,19 @@
 
 function initialize() {
   var search = document.getElementById('search')
+  var clear = document.getElementById('clear')
   // this is subject to changes, since it depends on jisho.org
   const root = 'http://jisho.org/search/'
+
   search.onclick = function() {
     var input = document.getElementById('input').value
     if (input != "") {
       chrome.tabs.create({url: root + input})
     }
+  }
+
+  clear.onclick = function() {
+    document.getElementById('input').value = ""
   }
 }
 
